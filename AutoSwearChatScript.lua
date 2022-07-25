e8a6e1d85a8d0735cd5e66daf522739b98d8bd7d
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-07-25 19:47:03 +0200

    Auto say [Censored]

---
+++ AutoSwearChatScript.lua
@@ -0,0 +1,12 @@
+while true do
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("gay", "All")
+	wait(2)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I rape children", "All")
+	wait(2)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("gayshit shit", "All")
+	wait(2)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("fuck", "All")
+	wait(2)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("motherfucker bitch fucking nigger", "All")	
+	wait(2)
+end
