c6d5d4ffb962bc296b70e66e58d0a1825a8731f0
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-07-25 19:46:10 +0200

    Removes the chat filter

---
+++ AntiHashtagChatScript.lua
@@ -0,0 +1,7 @@
+local chatservice = require(game.ServerScriptService.ChatServiceRunner.ChatService);
+chatservice.InternalApplyRobloxFilterNewAPI=function(self,sp,mes,textfilcon)
+	return true,false,mes 
+end;
+chatservice.InternalApplyRobloxFilter=function(self,sp,mes,toname) 
+	return mes 
+end
